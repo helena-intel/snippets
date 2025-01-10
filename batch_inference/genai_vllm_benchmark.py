4c8c994dc13e0f2cac3b817352a35e111fa079de
Helena Kloosterman <helena.kloosterman@intel.com>
2025-01-10 11:25:44 +0100

    Align max_num_batched_tokens with vLLM default

--- batch_inference/genai_vllm_benchmark.py
+++ batch_inference/genai_vllm_benchmark.py
@@ -10,10 +10,9 @@ import time
 import cpuinfo
 import openvino_genai as ov_genai
 from datasets import load_dataset
+from transformers import AutoConfig
 from vllm import LLM, SamplingParams
-
-# SETTINGS
-max_num_batched_tokens = 2000  # for GenAI
+from vllm.config import _get_and_verify_max_len
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--model_path", required=True)
@@ -25,15 +24,18 @@ parser.add_argument("--dataset", choices=("flores_101", "imdb", "gsm8k"), defaul
 parser.add_argument("--logfile", help="Optional path to log file. csv data is appended to the file")
 args = parser.parse_args()
 
+# Align max_num_batched_tokens with vLLM default
+model_config = AutoConfig.from_pretrained(args.model_path)
+max_num_batched_tokens = _get_and_verify_max_len(model_config, max_model_len=None, disable_sliding_window=True, sliding_window_len=None)
+
+
 if args.framework == "vllm":
     os.environ["VLLM_OPENVINO_KVCACHE_SPACE"] = str(args.memory_size)
-    pipe = LLM(model=args.model_path)
+    pipe = LLM(model=args.model_path, max_num_batched_tokens=max_num_batched_tokens)
     config = SamplingParams(temperature=0, max_tokens=args.max_new_tokens, ignore_eos=True)
 else:
     scheduler = ov_genai.SchedulerConfig()
     scheduler.cache_size = args.memory_size
-    # max_num_batched_tokens is 256 by default, for acceptable latency
-    # To improve throughput, set max_num_batched_tokens to a higher value
     scheduler.max_num_batched_tokens = max_num_batched_tokens
     pipe = ov_genai.ContinuousBatchingPipeline(args.model_path, scheduler, "CPU")
     config = pipe.get_config()
@@ -62,6 +64,7 @@ end_time = time.time()
 total_duration = end_time - start_time
 
 results = args.__dict__
+results["max_batched_tokens"] = max_num_batched_tokens
 results["cpu"] = cpuinfo.get_cpu_info().get("brand_raw")
 results["duration"] = round(total_duration, 2)
 
