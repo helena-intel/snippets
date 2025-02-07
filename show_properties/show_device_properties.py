95d5a7046e1ae427ff8b7eec14e8dc5e6115b2bd
helena <helena.kloosterman@intel.com>
2025-02-07 19:40:51 +0100

    Do not print properties with TypeError

--- show_properties/show_device_properties.py
+++ show_properties/show_device_properties.py
@@ -15,16 +15,19 @@ for device in [*core.available_devices, "AUTO"]:
     supported_properties = core.get_property(device, "SUPPORTED_PROPERTIES")
     for prop in supported_properties:
         if not prop == "SUPPORTED_PROPERTIES":
-            value = core.get_property(device, prop)
-            # read-only or read-write property
-            rorw = supported_properties[prop]
-            print(f"{prop} ({rorw}): {value}")
+            try:
+                value = core.get_property(device, prop)
+                # read-only or read-write property
+                rorw = supported_properties[prop]
+                print(f"{prop} ({rorw}): {value}")
+            except TypeError:
+                pass
     print()
 
 
 try:
     openvino_version = importlib.metadata.version("openvino")
-except:
+except Exception:
     # if OpenVINO is installed from archives, importlib.metadata fails
     openvino_version = ov.__version__
 
