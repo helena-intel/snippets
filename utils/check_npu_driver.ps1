9742ce00ffa68e7ca1bef2ef9827c7635d0e5926
Helena <helena.kloosterman@intel.com>
2025-07-02 20:16:58 +0200

    Get latest NPU driver version from website

--- utils/check_npu_driver.ps1
+++ utils/check_npu_driver.ps1
@@ -1,5 +1,6 @@
 # PowerShell script to check NPU driver version on Windows AI PC with Intel NPU. The script checks the currently
-# installed (and in-use) NPU driver against a hardcoded latest version and shows if the driver is up-to-date
+# installed (and in-use) NPU driver against the latest version and shows if the driver is up-to-date
+# This is work-in-progress, web-scraping to get the latest version is not expected to be very robust
 #
 # Run from either cmd.exe or PowerShell with (-NoProfile and -ExecutionPolicy Bypass are optional but included for broadest compatibility):
 # powershell.exe -NoProfile -ExecutionPolicy Bypass -File check_npu_driver.ps1
@@ -7,8 +8,18 @@
 # powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "iex (irm 'https://raw.githubusercontent.com/helena-intel/snippets/refs/heads/main/utils/check_npu_driver.ps1')"
 
 $deviceNameMatch = "AI Boost"
-$latestDriverVersion = "32.0.100.4082"
 $driverUpdateURL = "https://www.intel.com/content/www/us/en/download/794734/intel-npu-driver-windows.html"
+
+# Try to get the latest version from the driver URL. This may not be very robust, for now,
+# fall back to a hardcoded version if there are issues.
+$defaultVersion = "32.0.100.4082"
+$html = curl.exe -s $driverUpdateURL 2>$null
+
+if ($html -and $html -match '32\.0\.100\.\d+.*Latest') {
+    $latestDriverVersion = [regex]::Match($html, '32\.0\.100\.\d+').Value
+} else {
+    $latestDriverVersion = $defaultVersion
+}
 
 # Get current driver version
 $currentDriverVersion = (Get-PnpDevice |
